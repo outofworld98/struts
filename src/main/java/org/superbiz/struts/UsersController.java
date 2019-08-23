8b1659576fe9b79c6284f27d27ad0cbddc6fd059
sjlee5 <sjlee5@lsis.com>
2019-08-24 00:14:19 +0900

    modernization2 json

--- src/main/java/org/superbiz/struts/UsersController.java
+++ src/main/java/org/superbiz/struts/UsersController.java
@@ -1,12 +1,12 @@
 package org.superbiz.struts;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -58,5 +58,22 @@ public class UsersController {
     public String listUsers(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "displayUsers";
+    }
+
+    @ResponseBody
+    @GetMapping("/findIdData/{id}")
+    public ResponseEntity<User> read(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user != null) {
+            return new ResponseEntity(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @ResponseBody
+    @GetMapping("/listData")
+    public ResponseEntity<List<User>> list() {
+        return new ResponseEntity(userRepository.findAll(), HttpStatus.OK);
     }
 }
